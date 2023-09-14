7c8aec10822ba3b041598c7f05f689836e694035
AlonaHlobina <54394529+AlonaHlobina@users.noreply.github.com>
2023-09-14 14:22:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,3 +20,18 @@ document.write("Hello, world!");
 document.write(window.location.search);
 
 document.write(window.location.search);
+
+
+// It's a classic:
+document.write(window.location.search)
+
+// Here's a different one
+app.get('/some/path', function(req, res) {
+    let url = req.param('url'),
+        host = urlLib.parse(url).host;
+    // BAD: the host of `url` may be controlled by an attacker
+    let regex = /^((www|beta).)?example.com/;
+    if (host.match(regex)) {
+        res.redirect(url);
+    }
+});
