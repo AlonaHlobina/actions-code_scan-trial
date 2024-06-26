c142d091cbf5eed0ac63a9a3e6a4d217d5d2d6db
AlonaHlobina <54394529+AlonaHlobina@users.noreply.github.com>
2024-06-26 16:49:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -59,3 +59,27 @@ app.get('/some/path', function(req, res) {
         res.redirect(url);
     }
 });
+
+
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+var routes = require("./api/routes");
+routes(app);
+
+if (! module.parent) {
+  app.listen(port);
+}
+
+module.exports = app
+
+console.log("Server running on port " + port);
+
+document.write("Hello, world!");
+
+document.write(window.location.search);
+
